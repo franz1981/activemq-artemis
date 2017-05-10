12c34475f16a73d669c3307999e6fa03601c6cb3
Francesco Nigro <nigro.fra@gmail.com>
2017-05-10 21:26:22 +0200

    Test on IOPS fallancy of original TimedBuffer

--- tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/journal/impl/TimedBufferTest.java
+++ tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/journal/impl/TimedBufferTest.java
@@ -22,12 +22,14 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.activemq.artemis.api.core.ActiveMQBuffer;
 import org.apache.activemq.artemis.api.core.ActiveMQBuffers;
 import org.apache.activemq.artemis.core.io.IOCallback;
 import org.apache.activemq.artemis.core.io.buffer.TimedBuffer;
 import org.apache.activemq.artemis.core.io.buffer.TimedBufferObserver;
+import org.apache.activemq.artemis.core.journal.EncodingSupport;
 import org.apache.activemq.artemis.tests.util.ActiveMQTestBase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -121,6 +123,144 @@ public class TimedBufferTest extends ActiveMQTestBase {
          }
       } finally {
          timedBuffer.stop();
+      }
+
+   }
+
+   private static void flushThatTooks(long deadLine) {
+      while (System.nanoTime() < deadLine) {
+         //noop
+      }
+   }
+
+   @Test
+   public void shouldNotFlushUntilTimeout() throws InterruptedException {
+      final int bufferSize = 8;
+
+      //100 milliseconds of timeout
+      final int timeoutInNanos = (int) TimeUnit.MILLISECONDS.toNanos(100);
+      final int fixedFlushTime = timeoutInNanos;
+      final int messages = 10;
+      final List<String> errors = new ArrayList<>(messages);
+
+      class TestObserver implements TimedBufferObserver {
+
+         private final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+
+         //safe start
+         private long lastflushTime = System.nanoTime() - (timeoutInNanos + 1);
+
+         @Override
+         public void flushBuffer(final ByteBuffer buffer, final boolean sync, final List<IOCallback> callbacks) {
+            //test valid only when sync is called!
+            assert sync;
+            //fake the consume of the buffer
+            buffer.flip();
+            final int writtenSize = buffer.limit();
+            if (writtenSize > 0) {
+               //only real writes will be considered IO
+               final long now = System.nanoTime();
+               //THE NEW FLUSH CAN'T BE REQUESTED BEFORE TIMEOUT HAS BEEN EXPIRED!!!
+               final long interval = now - this.lastflushTime;
+               if (interval <= timeoutInNanos) {
+                  //not synchronized but uses callbacks::done to publish the values between threads
+                  errors.add("A flush has happened before the expected timeout! BROKEN IOPS LIMIT");
+               }
+               //a successful flush will take at least timeoutInNanos (optimistic) -> in a real disk depends at least on the size!!!
+               //spin wait to be precise :P
+               final long expectedFlushDeadline = now + timeoutInNanos;
+               flushThatTooks(expectedFlushDeadline);
+               //take the real one
+               this.lastflushTime = System.nanoTime();
+               //test not OS interruptions
+               assert lastflushTime >= expectedFlushDeadline;
+               buffer.clear();
+               callbacks.forEach(IOCallback::done);
+            }
+         }
+
+         /* (non-Javadoc)
+          * @see org.apache.activemq.artemis.utils.timedbuffer.TimedBufferObserver#newBuffer(int, int)
+          */
+         @Override
+         public ByteBuffer newBuffer(final int maxSize, final int limit) {
+            buffer.limit();
+            return buffer;
+         }
+
+         //infinite file: we are optimist
+         @Override
+         public int getRemainingBytes() {
+            return Integer.MAX_VALUE;
+         }
+      }
+
+      final AtomicLong flushesDone = new AtomicLong(0);
+
+      final IOCallback counterCallback = new IOCallback() {
+
+         private long done = 0;
+
+         @Override
+         public void done() {
+            this.done++;
+            flushesDone.lazySet(this.done);
+         }
+
+         @Override
+         public void onError(int errorCode, String errorMessage) {
+
+         }
+      };
+
+      final TimedBuffer timedBuffer = new TimedBuffer(bufferSize, timeoutInNanos, false);
+      timedBuffer.start();
+      //need to wait the flusher thread to start to not invalidate the test!
+      Thread.sleep(2_000);
+      try {
+         final EncodingSupport encodingSupport = new EncodingSupport() {
+
+            private final int encodedSize = bufferSize;
+
+            @Override
+            public int getEncodeSize() {
+               return bufferSize;
+            }
+
+            @Override
+            public void encode(ActiveMQBuffer buffer) {
+               //fake write, move only the write index
+               buffer.writerIndex(buffer.writerIndex() + encodedSize);
+            }
+
+            @Override
+            public void decode(ActiveMQBuffer buffer) {
+               throw new UnsupportedOperationException("impossible in this test!");
+            }
+         };
+         final int encodedSize = encodingSupport.getEncodeSize();
+         timedBuffer.setObserver(new TestObserver());
+         //the whole test should take more than...
+         final int expectedMinTestTime = messages * (2 * timeoutInNanos);
+         final long startTest = System.nanoTime();
+         for (int i = 0; i < messages; i++) {
+            final boolean canAddBytes = timedBuffer.checkSize(encodedSize);
+            assert canAddBytes;
+            timedBuffer.addBytes(encodingSupport, true, counterCallback);
+         }
+         //wait the last effective flush
+         while (flushesDone.get() != messages) {
+            //spin wait until the last is done
+         }
+         final long elapsedTestTime = System.nanoTime() - startTest;
+         Assert.assertTrue("BROKEN IOPS LIMIT " + errors.size() + " TIMES!", errors.isEmpty());
+         Assert.assertTrue("The test can't finish so early!", elapsedTestTime >= expectedMinTestTime);
+      } finally {
+         try {
+            timedBuffer.stop();
+         } catch (Throwable t) {
+            //discard any exception here to not mess with test exception logs
+         }
       }
 
    }
