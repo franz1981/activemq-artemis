4e7d5aa24ca23559ca2499531b9e984126441d6e
franz1981 <nigro.fra@gmail.com>
2020-09-17 12:38:21 +0200

    ARTEMIS-2823 centralize JDBC network timeout configuration

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
@@ -489,6 +489,24 @@ public class ActiveMQServerImpl implements ActiveMQServer {
       return this;
    }
 
+   private void configureJdbcNetworkTimeout() {
+      if (configuration.isPersistenceEnabled()) {
+         if (configuration.getStoreConfiguration() != null && configuration.getStoreConfiguration().getStoreType() == StoreConfiguration.StoreType.DATABASE) {
+            DatabaseStorageConfiguration databaseStorageConfiguration = (DatabaseStorageConfiguration) configuration.getStoreConfiguration();
+            databaseStorageConfiguration.setConnectionProviderNetworkTimeout(threadPool, databaseStorageConfiguration.getJdbcNetworkTimeout());
+         }
+      }
+   }
+
+   private void clearJdbcNetworkTimeout() {
+      if (configuration.isPersistenceEnabled()) {
+         if (configuration.getStoreConfiguration() != null && configuration.getStoreConfiguration().getStoreType() == StoreConfiguration.StoreType.DATABASE) {
+            DatabaseStorageConfiguration databaseStorageConfiguration = (DatabaseStorageConfiguration) configuration.getStoreConfiguration();
+            databaseStorageConfiguration.clearConnectionProviderNetworkTimeout();
+         }
+      }
+   }
+
    /*
     * Can be overridden for tests
     */
@@ -579,6 +597,9 @@ public class ActiveMQServerImpl implements ActiveMQServer {
 
       try {
          checkJournalDirectory();
+
+         // this would create the connection provider while setting the JDBC global network timeout
+         configureJdbcNetworkTimeout();
 
          nodeManager = createNodeManager(configuration.getNodeManagerLockLocation(), false);
 
@@ -1270,6 +1291,9 @@ public class ActiveMQServerImpl implements ActiveMQServer {
       if (!threadPoolSupplied) {
          this.threadPool = null;
       }
+
+      // given that threadPool can be garbage collected, need to clear anything that would make it leaks
+      clearJdbcNetworkTimeout();
 
       if (activationThread != null) {
          try {
